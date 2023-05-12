18d8dc85d71622d116368a791a495d26639482bb
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-05-12 14:05:27 +0100

    add bonus

--- main.js
+++ main.js
@@ -3,65 +3,57 @@ const selectContainer = document.getElementById('container');
 
 const arrayMembriDelTeam = [
     {
-        "nome": "Wayne Barnett",
-        "ruolo": "Founder & CEO",
-        "foto": "wayne-barnett-founder-ceo.jpg"
+        nome: "Wayne Barnett",
+        ruolo: "Founder & CEO",
+        foto: "wayne-barnett-founder-ceo.jpg"
     },
     {
-        "nome": "Angela Caroll",
-        "ruolo": "Chief Editor",
-        "foto": "angela-caroll-chief-editor.jpg"
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        foto: "angela-caroll-chief-editor.jpg"
     },
     {
-        "nome": "Walter Gordon",
-        "ruolo": "Office Manager",
-        "foto": "walter-gordon-office-manager.jpg"
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        foto: "walter-gordon-office-manager.jpg"
     },
     {
-        "nome": "Angela Lopez",
-        "ruolo": "Social Media Manager",
-        "foto": "angela-lopez-social-media-manager.jpg"
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        foto: "angela-lopez-social-media-manager.jpg"
     },
     {
-        "nome": "Scott Estrada",
-        "ruolo": "Developer",
-        "foto": "scott-estrada-developer.jpg"
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        foto: "scott-estrada-developer.jpg"
     },
     {
-        "nome": "Barbara Ramos",
-        "ruolo": "Graphic Designer",
-        "foto": "barbara-ramos-graphic-designer.jpg"
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        foto: "barbara-ramos-graphic-designer.jpg"
     }
 ];
 
-// console.log(wayne.nome, wayne.ruolo, wayne.foto);
-// selectContainer.innerHTML = wayne.nome;
-// selectContainer.innerHTML += wayne.ruolo;
-// selectContainer.innerHTML += wayne.foto;
 
+// Cicla l'array
 for (let i = 0; i < arrayMembriDelTeam.length; i++) {
-
     const indiceArray = arrayMembriDelTeam[i];
 
+    // crea 6 div con classe card
+    const newCard = document.createElement('div');
+    selectContainer.append(newCard);
+    newCard.innerHTML += `<img src="img/${arrayMembriDelTeam[i].foto}">`;
+    newCard.innerHTML += `<h3>${arrayMembriDelTeam[i].nome}</h3>`;
+    newCard.innerHTML += `<p>${arrayMembriDelTeam[i].ruolo}</p>`;
+
+
+    // cicla l'oggetto
     for (let key in indiceArray) {
         const indiceObject = indiceArray[key];
 
-        // scrivi console
+        // scrivi console le proprieta' dell'oggetto
         console.log(indiceObject);
 
-        // scrivi sul DOM
-        selectContainer.innerHTML += indiceObject;
 
     }
 }
-
-
-
-
-
-// for (let key in arrayMembriDelTeam) {
-//     console.log(key);
-// }
-
-
-
