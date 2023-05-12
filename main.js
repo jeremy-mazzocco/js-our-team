c0082adbc76d74babfedf4f143ef9884bcae550a
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-05-12 12:28:56 +0100

    add milestone 1

---
+++ main.js
@@ -0,0 +1,47 @@
+const arrayMembriDelTeam = [
+    {
+        "nome": "Wayne Barnett",
+        "ruolo": "Founder & CEO",
+        "foto": "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        "nome": "Angela Caroll",
+        "ruolo": "Chief Editor",
+        "foto": "angela-caroll-chief-editor.jpg"
+    },
+    {
+        "nome": "Walter Gordon",
+        "ruolo": "Office Manager",
+        "foto": "walter-gordon-office-manager.jpg"
+    },
+    {
+        "nome": "Angela Lopez",
+        "ruolo": "Social Media Manager",
+        "foto": "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        "nome": "Scott Estrada",
+        "ruolo": "Developer",
+        "foto": "scott-estrada-developer.jpg"
+    },
+    {
+        "nome": "Barbara Ramos",
+        "ruolo": "Graphic Designer",
+        "foto": "barbara-ramos-graphic-designer.jpg"
+    }
+]
+
+for (let i = 0; i < arrayMembriDelTeam.length; i++) {
+    console.log(arrayMembriDelTeam[i]);
+}
+
+
+
+
+
+// for (let key in arrayMembriDelTeam) {
+//     console.log(key);
+// }
+
+
+
